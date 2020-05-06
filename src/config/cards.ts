ab9c0d046e76e0b3b21f537f64ad3d7065966323
amullabaev <a.mullabaev@gmail.com>
2020-05-06 23:02:45 +0600

    Added data for cards

---
+++ src/config/cards.ts
@@ -0,0 +1,17 @@
+import { ICard } from '../components/Card/Card.interface';
+
+export const cards: ICard[] = [
+    {value: 0, color: 'blue'},
+    {value: 1, color: 'blue'},
+    {value: 2, color: 'blue'},
+    {value: 3, color: 'blue'},
+    {value: 5, color: 'blue'},
+    {value: 8, color: 'blue'},
+    {value: 13, color: 'green'},
+    {value: 21, color: 'green'},
+    {value: 34, color: 'green'},
+    {value: 55, color: 'green'},
+    {value: 89, color: 'green'},
+    {value: '?', color: 'yellow'},
+    {value: 'Pass', color: 'grey'},
+];
