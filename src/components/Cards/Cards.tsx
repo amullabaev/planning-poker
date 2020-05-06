0324a3d603d36641e426cbb517a4aceec847c7f1
amullabaev <a.mullabaev@gmail.com>
2020-05-06 23:27:11 +0600

    Added useState hook to handle selected card

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import { cards } from '../../config/cards';
-import { Card } from '../Card/Card';
+import React, {useState} from 'react';
+import {cards} from '../../config/cards';
+import {Card} from '../Card/Card';
 import './Cards.css'
+import {ICard} from "../Card/Card.interface";
 
 export const Cards: React.FC = () => {
+
+    const [selected, setSelected] = useState()
+
+    const onCardSelected = (card: ICard) => {
+        setSelected(card.value)
+    }
+
     return (
         <div className={'cards-row'}>
-            {cards.map(card => <Card card={card} key={card.value}/>)}
+            {cards.map(card =>
+                <Card card={card} key={card.value} cardSelected={onCardSelected} isSelected={selected === card.value}/>)
+            }
         </div>
     )
 }
