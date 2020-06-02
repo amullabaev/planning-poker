8bf5e18c3c298c6695de971089d37f840c09ae00
amullabaev <a.mullabaev@gmail.com>
2020-06-03 01:51:07 +0600

    added total score counter

--- src/components/Votes/Votes.tsx
+++ src/components/Votes/Votes.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Votes.css'
+import { cards } from '../../config/cards';
+import { ICard } from '../Card/Card.interface';
 
 export class Votes extends React.Component<any, any> {
 
@@ -24,12 +26,14 @@ export class Votes extends React.Component<any, any> {
         <button onClick={this.toggleVotes}>{this.state.showVotes ? 'Hide' : 'Show'} votes</button>
 
         { users.map((user: any) =>
-          <div className={'votes'}>
-            <div key={ user } style={{width: '100px'}}>{ user }:</div>
+          <div className={'votes'} key={ user } >
+            <div style={{width: '100px'}}>{ user }:</div>
             <div style={{width: '100px'}}>{ activeTask && this.getValue(user) }</div>
           </div>
 
         ) }
+        <br/>
+        <span>Total: {this.getTotalScore()}</span>
       </div>
     </>
   }
@@ -38,8 +42,25 @@ export class Votes extends React.Component<any, any> {
     this.setState({showVotes: !this.state.showVotes})
   }
 
+  private getTotalScore = () => {
+    let totalScore
+
+    if (this.props.scores.tasks && this.props.selectedTask) {
+      const values = Object.entries(this.props.scores.tasks[this.props.selectedTask])
+        .filter((i: any) => i[0] !== 'active' && Number.isInteger(i[1]))
+        .flatMap(i => i[1])
+      // @ts-ignore
+      const score = values.reduce((a, b) => a + b) / values.length
+      const cardValues = cards.map((card: any) => card.value)
+        .filter((i: any) => Number.isInteger(i))
+        .sort((a: number, b: number ) => +a - +b)
+      totalScore = cardValues.find(i => i >= score)
+    }
+
+    return this.state.showVotes && totalScore ? totalScore : 'n/a'
+  }
+
   private getValue = (user: any) => {
-    console.log(this.props.scores.tasks[this.props.selectedTask][user]);
     if (this.state.showVotes) {
       return this.props.scores.tasks[this.props.selectedTask][user]
     }
