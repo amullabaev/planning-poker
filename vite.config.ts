dee341adf5d7603795e1c8a4ce7b437ea8d78df6
Amirkhan Mullabaev <a.mullabaev@gmail.com>
2026-08-11 21:43:10 +0200

    Add .env config and proxy for a server

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  const protocol = env.VITE_API_PROTOCOL ?? 'http';
+  const host = env.VITE_API_HOST ?? 'localhost';
+  const port = env.VITE_API_PORT ?? '8000';
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  build: {
-    outDir: 'build'
-  }
+    build: {
+      outDir: 'build'
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: `${protocol}://${host}:${port}`,
+          changeOrigin: true
+        }
+      }
+    }
+  };
 });
